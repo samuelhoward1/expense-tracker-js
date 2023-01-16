2e2194d5a3ad78c2255a7224abdec6e21896efc0
Samuel Howard samuel.howard34@gmail.com <samuel.howard34@gmail.com>
2023-01-16 18:36:01 +0100

    drop down menu

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,20 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
-import React from "react";
+import ExpensesFilter from "./ExpensesFilter";
 
-function Expenses(props) {
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
+    <div>
     <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -27,6 +36,7 @@ function Expenses(props) {
         date={props.items[3].date}
       />
     </Card>
+    </div>
   );
-}
+};
 export default Expenses;
